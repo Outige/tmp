a1986dd4600d05dffe9807d409c6037ecdc4a8d6
Tieg <tiegosullivanpsnl@gmail.com>
2020-12-13 17:55:33 +0200

    ready to launch

--- sql_app/database.py
+++ sql_app/database.py
@@ -9,7 +9,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 # SQLALCHEMY_DATABASE_URL = 'sqlite:///' + os.path.join(basedir, 'sql_app.db')
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
-SQLALCHEMY_DATABASE_URL = "postgres://osgcfjjqxheapy:e74118db45e5c3a4098c9b761ed75e68187c7228907f9dd3fcf2b230a1bafd3d@ec2-50-17-218-108.compute-1.amazonaws.com:5432/d6o4j4vds51re5"
+SQLALCHEMY_DATABASE_URL = "postgres://ipehjzgtutjrdd:151db6d7c8c5ca0aeab88ec46166e159aef003ec004a4e6cf8e0075533bed1da@ec2-35-169-184-61.compute-1.amazonaws.com:5432/d1k2i8r28nki96"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL#, connect_args={"check_same_thread": False}
